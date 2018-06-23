497abe3db8b1afbd9457af88c232f5b61079dd40
Hack Reactor Student <student@hackreactor.com>
2018-06-22 17:31:31 -0700

    Pass all tests other than 'events' tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,22 @@
 class App {
   constructor() {
     this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+    this.rooms = [];
   }
 
   init() {
-    console.log(this + ' initialized');
-    this.fetch();
+    app.fetch();
   }
 
+  // adds a new message to messages
   send(message) {
     $.ajax({
-      url: this.server,
+      url: app.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
-        this.fetch();
+        app.renderMessage(message);
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
@@ -24,16 +24,21 @@ class App {
     });
   }
 
+  // add messages to site
   fetch() {
     $.ajax({
-      url: this.server,
+      url: app.server,
       type: 'GET',
+      data: "where=" + escape(JSON.stringify({"username": "zach"})),
       contentType: 'application/json',
       success: function (data) {
+        console.log(data);
         data.results.forEach(function(message) {
-          this.renderMessage(message);
+          if (!app.rooms.includes(message.roomname)) {
+            app.renderRoom(message);
+          }
+          app.renderMessage(message);
         });
-        console.log('chatterbox: Messages updated');
       },
       error: function (data) {
         console.error('chatterbox: Failed to recieve messages', data);
@@ -42,24 +47,64 @@ class App {
   }
 
   clearMessages() {
-    console.log($('.chat'));
-    $('.chat').each(function(message) {
-      console.log(message);
-    });
+    $('#chats').empty();
   }
 
+  // adds message to html
   renderMessage(message) {
     $('#chats').prepend(`<div class="chat">
                            <span class="username">${message.username}</span>
                            <br>
                            <span class="chat-text">${message.text}</span>
                          </div>`);
+    // add event click handler to .username elements
   }
 
-  renderRoom() {
-
+  renderRoom(message) {
+    app.rooms.push(message.roomname);
+    $('select').append(`<option value="${message.roomname}">
+                          ${message.roomname}
+                        </option>`);
+  }
+  
+  handleSubmit(message) {
+    app.send(message);
   }
 };
 
 var app = new App();
 app.init();
+
+$(document).ready(function() {
+  $("button").on("click", function() {
+    var message = {};
+    
+    message.username = getUsername(window.location.href);
+    message.text = getText();
+    message.roomname = getRoomname();
+
+    app.handleSubmit(message);
+  });
+});
+
+var getUsername = function(url) {
+  console.log(url);
+  for (var x = 0; x < url.length; x++) {
+    if (url[x] === '?' && url[x + 9] === '=') {
+      return url.slice(x + 10, url.length);
+    }
+  }
+};
+
+var getText = function() {
+  return $('input').val();
+};
+
+var getRoomname = function() {
+  var select = $('select')[0];
+  for (var i = 0; i < select.length; i++) {
+    if (select[i].selected === true) {
+      return select[i].value;
+    }
+  }
+};
